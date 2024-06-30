5ff96d05b9ea91f91724c30d148b81a2a6c8851d
DerekG29 <derekgrubbs29@gmail.com>
2024-06-30 17:02:48 -0400

    changed methods to use try/catch blocks

--- api/posts/posts-router.js
+++ api/posts/posts-router.js
@@ -5,43 +5,41 @@ const Posts = require('./posts-model');
 const router = express.Router();
 
 router.get('/', async (req, res) => {
-  Posts.find()
-    .then(posts => {
-      res.status(200).json(posts)
-    })
-    .catch(() => {
-      res.status(500).json({ message: "The posts information could not be retrieved" });
-    });
+  try {
+    const posts = await Posts.find();
+    res.status(200).json(posts);
+  } catch {
+    res.status(500).json({ message: "The posts information could not be retrieved" });
+  }
 });
 
-router.get('/:id', (req, res) => {
-  Posts.findById(req.params.id)
-    .then(post => {
-      if (!post) {
-        res.status(404).json({ message: "The post with the specified ID does not exist" });
-      } else {
-        res.status(200).json(post);
-      }
-    })
-    .catch(() => {
-      res.status(500).json({ message: "The post information could not be retrieved" });
-    });
+router.get('/:id', async (req, res) => {
+  try {
+    const id = req.params.id;
+    const post = await Posts.findById(id);
+    if (!post) {
+      res.status(404).json({ message: "The post with the specified ID does not exist" });
+    } else {
+      res.status(200).json(post)
+    }
+  } catch {
+    res.status(500).json({ message: "The post information could not be retrieved" });
+  }
 });
 
-router.post('/', (req, res) => {
-  const { title, contents } = req.body;
-  if (!title || !contents) {
-    res.status(400).json({ message: "Please provide title and contents for the post" });
-  } else {
-    Posts.insert({ title, contents })
-      .then(async ({ id }) => {
-        const post = await Posts.findById(id);
-        res.status(201).json(post);
-      })
-      .catch(() => {
-        res.status(500)
-          .json({ message: "There was an error while saving the post to the database" });
-      });
+router.post('/', async (req, res) => {
+  try {
+    const { title, contents } = req.body;
+    if (!title || !contents) {
+      res.status(400).json({ message: "Please provide title and contents for the post" });
+    } else {
+      const { id } = await Posts.insert({ title, contents });
+      const post = await Posts.findById(id);
+      res.status(201).json(post);
+    }
+  } catch {
+    res.status(500)
+      .json({ message: "There was an error while saving the post to the database" });
   }
 });
 
