1eced7b38e4d4f87bb72d41748fb6af7e5d8aa25
DerekG29 <derekgrubbs29@gmail.com>
2024-06-30 17:16:56 -0400

    Should be able to GET all comments for a post with a specific ID

--- api/posts/posts-router.js
+++ api/posts/posts-router.js
@@ -79,4 +79,19 @@ router.delete('/:id', async (req, res) => {
   }
 });
 
+router.get('/:id/comments', async (req, res) => {
+  try {
+    const id = req.params.id;
+    const post = await Posts.findById(id);
+    if (!post) {
+      res.status(404).json({ message: "The post with the specified ID does not exist" });
+    } else {
+      const comments = await Posts.findPostComments(id);
+      res.status(200).json(comments);
+    }
+  } catch {
+    res.status(500).json({ message: "The comments information could not be retrieved" });
+  }
+});
+
 module.exports = router;
